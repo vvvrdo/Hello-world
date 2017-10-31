1da9c8447b9fd6cdd8e445dd492ea5b6ff838cd5
vvvrdo <33194619+vvvrdo@users.noreply.github.com>
2017-10-31 13:21:12 +0400

    Add files via upload

---
+++ Weight converter/converter.js
@@ -0,0 +1,5 @@
+//document.getElementById('output').style.display = "none";
+const jjjjj = document.getElementById('inputNum'); jjjjj.addEventListener('click', inputFunct);
+function inputfunct(){
+    alert("yourmom")
+}
